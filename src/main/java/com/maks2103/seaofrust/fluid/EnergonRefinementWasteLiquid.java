9b001abbcba1708758fdb19ac6fb96bf9c699b1c
alesharik <alesharikReserv@yandex.ru>
2016-09-02 20:12:36 +0300

    The beginning

---
+++ src/main/java/com/maks2103/seaofrust/fluid/EnergonRefinementWasteLiquid.java
@@ -0,0 +1,11 @@
+package com.maks2103.seaofrust.fluid;
+
+import net.minecraft.block.BlockLiquid;
+import net.minecraft.block.material.Material;
+
+public class EnergonRefinementWasteLiquid extends BlockLiquid {
+
+    protected EnergonRefinementWasteLiquid() {
+        super(Material.water);
+    }
+}
