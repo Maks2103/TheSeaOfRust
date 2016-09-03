5f0b6cd1d931975d5bfaa56265b54b0d6bcd3e9a
alesharik <alesharikReserv@yandex.ru>
2016-09-03 19:04:35 +0300

    Small fixes

--- src/main/java/com/maks2103/seaofrust/block/BlockRustyIron.java
+++ src/main/java/com/maks2103/seaofrust/block/BlockRustyIron.java
@@ -4,6 +4,10 @@ package com.maks2103.seaofrust.block;
 import com.maks2103.seaofrust.SeaOfRust;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class BlockRustyIron extends Block {
     protected BlockRustyIron() {
@@ -15,5 +19,40 @@ public class BlockRustyIron extends Block {
         setHardness(5.0F);
         setResistance(10.0F);
         setHarvestLevel("pickaxe", 1);
+        setTickRandomly(true);
+    }
+
+    @Override
+    public void updateTick(World world, int x, int y, int z, Random random) {
+        for(int i = 0; i < 6; i++) {
+            if(random.nextInt(10) == 0) {
+                switch (i) {
+                    case 0:
+                        if(world.getBlock(x, y + 1, z) == Blocks.iron_block)
+                            world.setBlock(x, y + 1, z, ModBlocks.rustyIron);
+                        break;
+                    case 1:
+                        if(world.getBlock(x, y + 1, z) == Blocks.iron_block)
+                            world.setBlock(x, y - 1, z, ModBlocks.rustyIron);
+                        break;
+                    case 2:
+                        if(world.getBlock(x, y + 1, z) == Blocks.iron_block)
+                            world.setBlock(x + 1, y, z, ModBlocks.rustyIron);
+                        break;
+                    case 3:
+                        if(world.getBlock(x, y + 1, z) == Blocks.iron_block)
+                            world.setBlock(x - 1, y, z, ModBlocks.rustyIron);
+                        break;
+                    case 4:
+                        if(world.getBlock(x, y + 1, z) == Blocks.iron_block)
+                            world.setBlock(x, y, z + 1, ModBlocks.rustyIron);
+                        break;
+                    case 5:
+                        if(world.getBlock(x, y + 1, z) == Blocks.iron_block)
+                            world.setBlock(x, y, z - 1, ModBlocks.rustyIron);
+                        break;
+                }
+            }
+        }
     }
 }
