be1993dd4cd60e571f110fa8fb2a73c66d18c5bd
Maks2103 <maks2103.romanov@yandex.ru>
2017-02-16 00:42:33 +0300

    Removed commented code.

--- src/main/java/com/maks2103/seaofrust/item/ItemEnergonSword.java
+++ src/main/java/com/maks2103/seaofrust/item/ItemEnergonSword.java
@@ -22,35 +22,10 @@ public class ItemEnergonSword extends ItemSword {
         return material != null && material.getItem() == ModItems.CHUNK_OF_RUST;
     }
 
-    //    @SideOnly(Side.CLIENT)
-//    public boolean isFull3D() {
-//        return true;
-//    }
-//
     @Override
     public Multimap getItemAttributeModifiers() {
         Multimap multimap = HashMultimap.create();
         multimap.put(SharedMonsterAttributes.attackDamage.getAttributeUnlocalizedName(), new AttributeModifier(field_111210_e, "Weapon modifier", 15, 0));
         return multimap;
     }
-//    public boolean onBlockDestroyed(ItemStack p_150894_1_, World p_150894_2_, Block p_150894_3_, int p_150894_4_, int p_150894_5_, int p_150894_6_, EntityLivingBase p_150894_7_)
-//    {
-//        if ((double)p_150894_3_.getBlockHardness(p_150894_2_, p_150894_4_, p_150894_5_, p_150894_6_) != 0.0D)
-//        {
-//            p_150894_1_.damageItem(2, p_150894_7_);
-//        }
-//
-//        return true;
-//    }
-//
-//    public EnumAction getItemUseAction(ItemStack p_77661_1_)
-//    {
-//        return EnumAction.block;
-//    }
-//
-//    public ItemStack onItemRightClick(ItemStack p_77659_1_, World p_77659_2_, EntityPlayer p_77659_3_)
-//    {
-//        p_77659_3_.setItemInUse(p_77659_1_, this.getMaxItemUseDuration(p_77659_1_));
-//        return p_77659_1_;
-//    }
 }
